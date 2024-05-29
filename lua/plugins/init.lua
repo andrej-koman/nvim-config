ea2840623af1e8bde51a3891d6b632a694a3ec72
andrej-koman <andrej.koman123@gmail.com>
2024-05-29 18:21:43 +0200

    error - oopise

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -42,25 +42,5 @@ return {
     build = function()
       require("nvim-treesitter.install").update { with_sync = true }()
     end,
-    config = function(plug, config)
-      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-
-      parser_config.blade = {
-        install_info = {
-          url = "https://github.com/EmranMR/tree-sitter-blade",
-          files = { "src/parser.c" },
-          branch = "main",
-        },
-        filetype = "blade",
-      }
-
-      vim.filetype.add {
-        pattern = {
-          [".*%.blade%.php"] = "blade",
-        },
-      }
-
-      require(plug.main).setup(config)
-    end,
   },
 }
